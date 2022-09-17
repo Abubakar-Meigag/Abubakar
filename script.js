cf0e6b903608951e44f1a3a07cdd759341ca8105
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2022-09-17 13:52:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,23 @@ test("There are at least 500 words on the page", () => {
 
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
+
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.setAttribute("href", 'http://www.codeyourfuture.io');
+new_a.innerText = 'CodeYourFuture';
+
+new_li.appendChild(new_a);
+let new_li2 = document.createElement('li');
+let new_a2 = document.createElement('a');
+new_a2.setAttribute("href", 'http://www.google.com');
+new_a2.innerText = 'Google';
+
+new_li2.appendChild(new_a2);
+
+new_li.appendChild(new_li2);
+
+element_list.appendChild(new_li);
+
