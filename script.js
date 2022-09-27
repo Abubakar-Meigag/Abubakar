0e8100ef17717c03c821715111317bf0e6f3a53b
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2022-09-27 16:42:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,35 +34,29 @@ document.getElementById('next').addEventListener('click', (event) => {
 
 });
 
-
-var images;
-
-
-images = ['https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=2400&h=-1&s=1', 'https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg', 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg'];
-let element_carousel = document.getElementById('carousel');
-element_carousel.setAttribute("src", images[0]);
+var img;
 
 
-document.getElementById('previous_button').addEventListener('click', (event) => {
-  let element_carousel2 = document.getElementById('carousel');
-  images.unshift(images.pop());
-  element_carousel2.setAttribute("src", images[0]);
+img = ['https://ied.eu/wp-content/uploads/2018/08/idea.png', 'https://i0.wp.com/tcbassociates.com/wp-content/uploads/2020/07/photo-of-people-doing-handshakes-3183197-scaled.jpg?resize=1132%2C509&ssl=1', 'https://assets.entrepreneur.com/content/3x2/2000/20171215195850-GettyImages-508066399.jpeg?auto=webp&quality=95&crop=16:9&width=675', 'https://www.freshbooks.com/wp-content/uploads/unique-business-ideas-from-around-world.jpg', 'https://media0.giphy.com/media/pxrr47YT7QcLnqhrQe/giphy.gif?cid=ecf05e47v9zanf8cipsvnax8m1acxa39q3y8ev3nu1529r1c&rid=giphy.gif&ct=g'];
 
-});
+let element_pic = document.getElementById('pic');
+img.push(img[0]);
+element_pic.setAttribute("src", img.shift());
 
-document.getElementById('previous_button').addEventListener('click', (event) => {
-  let element_image = document.getElementById('image');
-  let new_img = document.createElement('img');
-  images.unshift(images.pop());
-  new_img.setAttribute("src", images[0]);
 
-  element_image.appendChild(new_img);
+document.getElementById('previous').addEventListener('click', (event) => {
+  let element_pic2 = document.getElementById('pic');
+  img.unshift(img.slice(-1)[0]);
+  element_pic2.setAttribute("src", img.shift());
 
 });
 
-document.getElementById('next_button').addEventListener('click', (event) => {
-  let element_carousel3 = document.getElementById('carousel');
-  images.push(images.shift());
-  element_carousel3.setAttribute("src", images[0]);
+document.getElementById('next').addEventListener('click', (event) => {
+  let element_pic3 = document.getElementById('pic');
+  img.push(img[0]);
+  element_pic3.setAttribute("src", img.shift());
 
 });
+
+
+
