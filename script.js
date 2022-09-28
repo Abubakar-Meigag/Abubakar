6f44143e0a54027aed1a6cc33b5d243144b4dd29
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2022-09-28 12:13:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,85 @@
-new_li.appendChild(new_a);
-let new_li2 = document.createElement('li');
-let new_a2 = document.createElement('a');
-new_a2.setAttribute("href", 'http://www.google.com');
-new_a2.innerText = 'Google';
+/* =================
+  TESTS, LOOK AT THESE
+  Reading tests will always help you discover your requirements.
+  You can make this window bigger. 
+   ===================
+*/
 
-new_li2.appendChild(new_a2);
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
 
-new_li.appendChild(new_li2);
+/* =================
+  FIND ELEMENTS
+  These are all the elements we will look for.
+   ===================
+*/
+const getHeader = document.querySelectorAll("header"),
+  getH1 = document.querySelectorAll("h1"),
+  getSiteHeader = document.querySelectorAll(".c-site-header"),
+  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+  getMain = document.querySelectorAll("main"),
+  getFooter = document.querySelectorAll("footer"),
+  getSiteFooter = document.querySelectorAll(".c-site-footer"),
+  getIFrame = document.querySelectorAll("iframe"),
+  getImage = document.querySelectorAll("img"),
+  getWords = document.body.innerText;
+
+/* =================
+   ASSERTIONS 
+   These are the things we check are true about your page.
+   Read and update your HTML to discover the requirements.
+   The tests will run every time you update your code.
+   ===================
+*/
+test("There is at least one header element", () => {
+  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one h1", () => {
+  expect(getH1.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one header element with the class c-site-header", () => {
+  expect(getSiteHeader.length).toBe(1);
+});
+test("There is a nav element with an aria-label of Main Site Links.", () => {
+  expect(getAria.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one main element", () => {
+  expect(getMain.length).toBe(1);
+});
+test("There is at least one footer element", () => {
+  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one footer element with the class c-site-footer", () => {
+  expect(getSiteFooter.length).toBe(1);
+});
+test("There is embedded video", () => {
+  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one image", () => {
+  expect(getImage.length).toBeGreaterThanOrEqual(1);
+});
+test("There are at least 500 words on the page", () => {
+  expect(getWords.length).toBeGreaterThanOrEqual(500);
+});
+
+const console = document.getElementById("tests");
+prettify.toHTML(run(), console);
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+new_li.innerText = getNumberOrString(document.getElementById('text').value);
 
 element_list.appendChild(new_li);
-
-var img;
-
-
-img = ['https://ied.eu/wp-content/uploads/2018/08/idea.png', 'https://i0.wp.com/tcbassociates.com/wp-content/uploads/2020/07/photo-of-people-doing-handshakes-3183197-scaled.jpg?resize=1132%2C509&ssl=1', 'https://assets.entrepreneur.com/content/3x2/2000/20171215195850-GettyImages-508066399.jpeg?auto=webp&quality=95&crop=16:9&width=675', 'https://www.freshbooks.com/wp-content/uploads/unique-business-ideas-from-around-world.jpg', 'https://media0.giphy.com/media/pxrr47YT7QcLnqhrQe/giphy.gif?cid=ecf05e47v9zanf8cipsvnax8m1acxa39q3y8ev3nu1529r1c&rid=giphy.gif&ct=g'];
-
-let element_pic = document.getElementById('pic');
-img.push(img[0]);
-element_pic.setAttribute("src", img.shift());
-
-
-document.getElementById('previous').addEventListener('click', (event) => {
-  let element_pic2 = document.getElementById('pic');
-  img.unshift(img.slice(-1)[0]);
-  element_pic2.setAttribute("src", img.pop());
-
-});
-
-document.getElementById('next').addEventListener('click', (event) => {
-  let element_pic3 = document.getElementById('pic');
-  img.push(img[0]);
-  element_pic3.setAttribute("src", img.shift());
-
-});
-
-<p>Enter your <u>earnings</u> with a <mark>positive</mark> number and your <u>expenses</u> with <mark>negative</mark> number
-</p>      
-<ul id="list1"></ul>
-<ul id="list2"></ul>
-
-<input id="number" placeholder="enter your number here"> 
-
-<button id="button">submit </button>
-<hr>
-<button id="btn1">press to find total earnings</button>
-<br>
-<br>
-<button id="btn2">press to find total expenses</button>
-<br>
-<br>
-<button id="btn3">press to find balance</button>
-<br>
-<br>
-
-
-
-
-
